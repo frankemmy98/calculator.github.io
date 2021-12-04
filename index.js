2895ef2fe9a1f119cfce370203ca066eaf1a2b3b
hanxolo <27387086+hanxolo99@users.noreply.github.com>
2021-12-04 22:03:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-    var number1 = parseFloat(document.querySelector("#input1").value);
-    var number2 = parseFloat(document.querySelector("#input2").value);
-    var numberOfOperators = document.querySelectorAll(".operators").length;
 
-    for(i = 0; i < numberOfOperators; i++){
-           document.querySelectorAll(".operators")[i].addEventListener("click", function(){
-                  calculate(this.innerHTML);
-           });
+    
+    let field1 = document.querySelector("#input1") 
+    let field2 = document.querySelector("#input2")
+    let field3 = document.querySelector('#result')
+    let my_operators = Array.from(document.getElementsByClassName('operators'))
+    
+    my_operators.forEach((operator)=>{
+        operator.addEventListener('click', ()=>{
+            dostuff(operator.innerHTML)
+        })
+    })
+
+    function dostuff(operand){
+        if(field1.value && field2.value){
+            let answer;
+            if(operand == '+'){
+                answer = parseFloat(field1.value) + parseFloat(field2.value)
+            }else if(operand == '-'){
+                answer = parseFloat(field1.value) - parseFloat(field2.value)
+            }else if(operand === '/'){
+                answer = parseFloat(field1.value) / parseFloat(field2.value)
+            }else if(operand === '*'){
+                answer = parseFloat(field1.value) * parseFloat(field2.value)
+            }
+
+            field3.value = answer
+        }else{
+            alert('Fields cannot be empty')
+        }
+        
     }
-
-function calculate(){
-    var operate = numberOfOperators.value;
-
-    switch(this.innerHTML){
-
-       case(operate === "+"):
-                 document.querySelector("#result").value = number1 + number2;
-                break;
-       case(operate === "-"):
-                 document.querySelector("#result").value = number1 - number2;
-                break;
-       case(operate === "/"):
-                 document.querySelector("#result").value = number1 / number2;
-                break;
-       case(operate === "*"):
-                 document.querySelector("#result").value = number1 * number2;
-                break;
-       default: console.log(calculate);
-    }
-}
-
